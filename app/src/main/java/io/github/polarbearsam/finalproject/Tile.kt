76aeb646ea52fdfdf4e8809b833750d291a84a2e
polarbearsam <112090381+polarbearsam@users.noreply.github.com>
2024-04-15 13:23:23 -0400

    Added initial code for Tile

--- app/src/main/java/io/github/polarbearsam/finalproject/Tile.kt
+++ app/src/main/java/io/github/polarbearsam/finalproject/Tile.kt
@@ -1,4 +1,11 @@
 package io.github.polarbearsam.kernelpop
 
-class Tile {
+class Tile(isMine: Boolean) {
+    var num : Int = 0
+
+    init {
+        if (isMine) {
+            num = 9
+        }
+    }
 }
