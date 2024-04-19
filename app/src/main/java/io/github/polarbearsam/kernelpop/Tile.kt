76b0205bc308e5b6d5f5b0b02530d3e2bd9aab90
polarbearsam <112090381+polarbearsam@users.noreply.github.com>
2024-04-19 13:08:46 -0400

    Added isFlagged to Tile.kt

--- app/src/main/java/io/github/polarbearsam/kernelpop/Tile.kt
+++ app/src/main/java/io/github/polarbearsam/kernelpop/Tile.kt
@@ -2,6 +2,7 @@ package io.github.polarbearsam.kernelpop
 
 // An object for every tile on the game board.
 class Tile() {
+    var isFlagged = false
     var isVisible = false
     var num : Int = 0
         set(value) { // Limits allowed values for a tile to 0 through 9.
